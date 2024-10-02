ac9601865f8703d7569612e5050dd13ba1f0a638
Matheus França <matheus.franca@opovo.com.br>
2024-10-02 11:07:14 -0300

    feat: add format currency

---
+++ frontend/src/core/utils/Currency.ts
@@ -0,0 +1,12 @@
+export class Currency {
+  static format(
+    value: number,
+    location: string = "pt-BR",
+    currency: string = "BRL"
+  ): string {
+    return (value ?? 0).toLocaleString(location, {
+      style: "currency",
+      currency,
+    });
+  }
+}
